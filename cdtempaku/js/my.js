7f85c62c8d0b2c5f74c701e9a8a735dbe16861cd
littlewing <keshin.skywalker@gmail.com>
2015-07-11 22:19:36 +0900

    カルーセル実装途中

--- cdtempaku/js/my.js
+++ cdtempaku/js/my.js
@@ -1,2 +1,86 @@
-alert("hello");
+
+var studioid = "1188244";
+var page = 1;
+
+
+var apiurl = "https://scratch.mit.edu/site-api/projects/in/";
+var projecturl_prefix = "//scratch.mit.edu/projects/embed/";
+var projecturl_surffix = "/?autostart=true";
+
+$(function () {
+
+	$("#content").hide();
+
+	url = apiurl + studioid + "/" + page + "/";
+
+	$.ajax({
+		url: url,
+		dataType:"html",
+		success: function(data) {
+		console.log(url);
+			var h = $(data);
+			$("#content").append(h);
+
+			$("#content").find(".title").find("a").each(function(i,elem ){
+
+				projectid = $(elem).attr("href").match(/\d+/); ;
+				projecttitle = $(elem).text();
+
+					console.log( i+"href=" + projectid);
+
+					var prjtag = makeEmbedTag(projectid,projecttitle);
+
+					console.log("prj=" + prjtag);
+
+					$("#content2").append(prjtag);
+				});
+
+
+				$('.single-item').slick({
+					  autoplay: true,
+						autoplaySpeed: 2000,
+						adaptiveHeight: true,
+						dots: true,
+						infinite: true,
+						speed: 300,
+						centerMode: true,
+						variableWidth: true
+						});
+		}	, 
+		error: function(xhr, status, err) {
+			$('#content').html('エラー発生');
+		} // 通信失敗時は<div>要素にメッセージを表示
+
+	});
+	//});
+
+});
+
+function makeEmbedTag(id,text){
+
+	var d = $("<div></div>",{
+		id:id,
+		"class":"project"
+	});
+	d.append("<h3 class=\"projecttitle\">"+text+"</h3>");
+
+	var emb = $("<iframe></iframe>",{
+		allowtransparency:true,
+		width:485,
+		height:402,
+		src: projecturl_prefix+projectid+projecturl_surffix,
+		frameborder:0,
+	}	);
+
+
+	$(d).append(emb);
+
+	return d;
+
+}
+
+function initialize_lazyload(){
+}
+
+
 
