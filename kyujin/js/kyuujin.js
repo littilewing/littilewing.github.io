973189a721caa8ea62a97328d37dfb296e9475f8
littlewing <hayashi.tomoki@outlook.com>
2015-11-17 16:18:46 +0900

    s

--- kyujin/js/kyuujin.js
+++ kyujin/js/kyuujin.js
@@ -2,4 +2,20 @@ $("#seachbtn").click(function () {
 		$("#seachform").submit();
 
 });
+$(document).ready(function() {
+		var pagetop = $('.pagetop');
+		$(window).scroll(function () {
+				if ($(this).scrollTop() > 400) {
+						pagetop.fadeIn();
+				} else {
+						pagetop.fadeOut();
+				}
+		});
+		pagetop.click(function () {
+				$('body, html').animate({ scrollTop: 0 }, 500);
+				return false;
+		});
+});
+
+
 
